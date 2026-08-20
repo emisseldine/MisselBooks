b9a5f864d6c0f53a67149d1ab6aaa3c47db71019
Andrew Misseldine <55810834+emisseldine@users.noreply.github.com>
2026-08-20 16:37:53 -0600

    Create svd.py

---
+++ LinearAlgebra/code/svd.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+A = np.array([[-9,18,54],		# Define matrix A
+              [-38,-49,-22]])
+
+U,sigma,VT = np.linalg.svd(A)	# Perform Singular Value Decomposition (SVD)
+print(U,"\n",							# Print the results
+      np.diag(sigma),"\n",
+      VT.T,"\n")
