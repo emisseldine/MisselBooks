7558141a0815fc93e45d430f7e38f71b0ee76424
Andrew Misseldine <55810834+emisseldine@users.noreply.github.com>
2026-08-24 09:52:35 -0600

    Create function.py

---
+++ LinearAlgebra/code/function.py
@@ -0,0 +1,14 @@
+square = lambda x : x**2
+
+print(square(5))   
+
+def power(base, exponent=2):
+    return base ** exponent
+
+print(power(5))      # 25 (uses default exponent = 2)
+print(power(5, 3))   # 125
+
+def greet(name):
+    print("Hello,", name)
+
+greet("Alice")   # prints "Hello, Alice"
